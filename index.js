29caf81bb634c883a7bf222c61bc0b964b0c5a88
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-12-13 15:30:28 +0200

    Changed greeting.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there' });
+    res.send({ bye: 'bye'});
 });
 
 const PORT = process.env.PORT || 5000;
