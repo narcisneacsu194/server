f2939759ff8b490d53ee8bcf57e5fd0eb0c255c6
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-12-27 11:15:59 +0200

    Added react front-end components.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
+import Landing from './Landing';
+import * as actions from '../actions';
+
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-       <div>
-         <Header/>
-         <Route exact path="/" component={Landing} />
-         <Route exact path="/surveys" component={Dashboard}/>
-         <Route path="/surveys/new" component={SurveyNew}/>
-       </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount(){
+    this.props.fetchUser();
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <BrowserRouter>
+         <div>
+           <Header/>
+           <Route exact path="/" component={Landing} />
+           <Route exact path="/surveys" component={Dashboard}/>
+           <Route path="/surveys/new" component={SurveyNew}/>
+         </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 };
 
-export default App;
+export default connect(null, actions)(App);
