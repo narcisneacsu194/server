770a1411d8631936d4cb2c0b1bddb842818c7967
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-12-29 18:02:58 +0200

    Finished implementing the billing feature.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -14,8 +14,9 @@ class Header extends Component {
         return <li><a href="/auth/google">Login with Google</a></li>;
       default: 
         return [
-          <li><Payments/></li>,
-          <li><a href="/api/logout">Logout</a></li>
+          <li key="1"><Payments/></li>,
+          <li key="2" style={{ margin: '0 10px'}}>Credits: { this.props.auth.credits }</li>,
+          <li key="3"><a href="/api/logout">Logout</a></li>
         ]
     }
   }
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class Payments extends Component{
+class Payments extends Component{
     render(){
         return (
             <StripeCheckout
               name='Emaily'
               description='$5 for 5 email credits'
               amount={500}
-              token={ (token) => console.log(token) }
+              token={ token => this.props.handleToken(token) }
               stripeKey={ process.env.REACT_APP_STRIPE_KEY }
             >
               <button className="btn">
@@ -18,3 +20,5 @@ export default class Payments extends Component{
         );
     }
 }
+
+export default connect(null, actions)(Payments);
