94c32438c93404adbdb833a4f503bc9a6072f296
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-02-16 19:44:26 +0200

    Almost finished working on the client and server side of the application.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,10 +4,9 @@ import { connect } from 'react-redux';
 
 import Header from './Header';
 import Landing from './Landing';
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 import * as actions from '../actions';
-
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
 
 class App extends Component {
 
@@ -17,18 +16,20 @@ class App extends Component {
 
   render(){
     return (
-      <div className="container">
         <BrowserRouter>
-         <div>
+         <div className="container">
            <Header/>
-           <Route exact path="/" component={Landing} />
-           <Route exact path="/surveys" component={Dashboard}/>
-           <Route path="/surveys/new" component={SurveyNew}/>
+           <Route exact path="/" component={this.props.auth ? Dashboard : Landing} />
+           <Route exact path="/surveys" component={this.props.auth ? Dashboard : Landing}/>
+           <Route path="/surveys/new" component={this.props.auth ? SurveyNew : Landing}/>
          </div>
         </BrowserRouter>
-      </div>
     );
   }
 };
 
-export default connect(null, actions)(App);
+function mapStateToProps({ auth }){
+  return { auth };
+}
+
+export default connect(mapStateToProps, actions)(App);
---
+++ routes/surveyRoutes.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const Survey = mongoose.model('surveys');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
+module.exports = app => {
+
+  app.post('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ( { email: email.trim() } )),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;  
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (error) {
+      res.status(422).send(error);
+    }
+  });
+};
