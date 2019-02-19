641ef7ccdc34269df50359ed34589e88e44df3ef
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-02-19 14:52:09 +0200

    Handled webhook data coming from the sendgrid application.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,8 +10,39 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
 
-  app.post('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+     .map(({ url, email }) => {
+      const match = p.test(new URL(url).pathname);
+      if(match){
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    })
+    .compact()
+    .uniqBy('email', 'surveyId')
+    .each(({ surveyId, email, choice }) => {
+      Survey.updateOne({
+        _id: surveyId,
+        recipients: {
+            $elemMatch: {
+                email: email, responded: false
+            }
+        }
+    }, {
+      $inc: { [choice]: 1 },
+      $set: { 'recipients.$.responded': true },
+      lastResponded: new Date()
+     }).exec();
+    })
+    .value();
+
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
